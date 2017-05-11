d8c60a1d91a7127d4081099611b5676198ececf2
Alan <Alan@10.121.38.71>
2017-05-11 08:38:48 -0500

    Moved .biom files into theyr own directory

--- src/com/apcs/grassland/BiomData.java
+++ src/com/apcs/grassland/BiomData.java
@@ -15,14 +15,16 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- * This class holds data about a specific biom. It will load it's properties from a file with the name given in the constructor plus the extension .biom
+ * This class holds data about a specific biom. It will load it's properties
+ * from a file with the name given in the constructor plus the extension .biom
  */
 public class BiomData {
-    public BiomData(String name){
+
+    public BiomData(String name) {
         this.name = name;
         props = new Properties();
-        File lFile = new File(name + ".biom");
-        if(lFile.exists()){
+        File lFile = getFileForName(name);
+        if (lFile.exists()) {
             FileInputStream fis = null;
             try {
                 fis = new FileInputStream(lFile);
@@ -42,25 +44,33 @@ public class BiomData {
     }
     private String name;
     private Properties props;
-    public String getName(){
+
+    public String getName() {
         return name;
     }
-    public float getHeightCoefficient(){
+
+    public float getHeightCoefficient() {
         return Float.parseFloat(props.getProperty("height"));
     }
-    public float getPercipCoefficient(){
+
+    public float getPercipCoefficient() {
         return Float.parseFloat(props.getProperty("percip"));
     }
-    
+
+    private static File getFileForName(String name) {
+        return new File("bioms" + System.getProperty("file.separator") + name + ".biom");
+    }
+
     /**
-     * Call this function from some debug method only once to create a fresh biom file
-     * 
+     * Call this function from some debug method only once to create a fresh
+     * biom file
+     *
      * @param name name of the biom, and the file which will be created
      */
-    public static void createNewDatafile(String name){
+    public static void createNewDatafile(String name) {
         FileOutputStream fos = null;
         try {
-            File lFile = new File(name + ".biom");
+            File lFile = getFileForName(name);
             Properties np = new Properties();
             np.setProperty("height", "1");
             np.setProperty("percip", "1");
