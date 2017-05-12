ddfa7c2d3b0f678e41694b9be7668f73c7121bfc
Alan <Alan@10.121.38.71>
2017-05-12 11:26:25 -0500

    fixed import erroprs for skybox class

--- src/com/apcs/grassland/skybox/SkyBox.java
+++ src/com/apcs/grassland/skybox/SkyBox.java
@@ -5,19 +5,72 @@
  */
 package com.apcs.grassland.skybox;
 
-import com.jme3.app.state.
+import com.jme3.app.Application;
+import com.jme3.app.state.AppState;
+import com.jme3.app.state.AppStateManager;
+import com.jme3.renderer.RenderManager;
 
 /**
  *
  * @author PhillipKobylinski
  */
-public class SkyBox extends GameState {
-    private String 
+public class SkyBox implements AppState{
+   
     public SkyBox() {
         
     }
     
     public void use() {
         
+    }
+
+    @Override
+    public void initialize(AppStateManager stateManager, Application app) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean isInitialized() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setEnabled(boolean active) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean isEnabled() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void stateAttached(AppStateManager stateManager) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void stateDetached(AppStateManager stateManager) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void update(float tpf) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void render(RenderManager rm) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void postRender() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void cleanup() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
